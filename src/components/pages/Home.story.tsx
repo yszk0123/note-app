8b6b48d48e392b809aae3292a6a122bde184df33
Yuji Suzuki <y0suzuki0123@gmail.com>
2018-05-04 18:54:58 +0900

    chore: fix story links

--- src/components/pages/Home.story.tsx
+++ src/components/pages/Home.story.tsx
@@ -3,5 +3,5 @@ import { storiesOf } from '@storybook/react';
 import * as React from 'react';
 
 storiesOf('Home', module)
-  .add('one', () => <div onClick={linkTo('two')}>two</div>)
-  .add('two', () => <div onClick={linkTo('one')}>two</div>);
+  .add('one', () => <div onClick={linkTo('Home', 'two')}>one</div>)
+  .add('two', () => <div onClick={linkTo('Home', 'one')}>two</div>);
